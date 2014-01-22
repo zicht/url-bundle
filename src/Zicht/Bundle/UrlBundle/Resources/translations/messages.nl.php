2d581d6a3dce3079904e45da9b18cb0e1d5a4303
Gerard van Helden <gerard@zicht.nl>
2014-01-22 10:30:00 +0000

    added some translation
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtUrlBundle/trunk@30966 0f35748b-8228-47f0-bbea-889afbbd9a42

---
+++ src/Zicht/Bundle/UrlBundle/Resources/translations/messages.nl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Gerard van Helden <gerard@zicht.nl>
+ * @copyright Zicht online <http://zicht.nl>
+ */
+
+return array(
+    'Public Url' => 'Publieke URL',
+    'Internal Url' => 'Interne URL',
+);
