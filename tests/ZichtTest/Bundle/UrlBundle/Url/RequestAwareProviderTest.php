cc55a6d2cf97220f73d768fb323eb6702a6c97ca
Gerard van Helden <gerard@zicht.nl>
2014-02-26 16:10:28 +0000

    added test case
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtUrlBundle/trunk@32929 0f35748b-8228-47f0-bbea-889afbbd9a42

--- tests/ZichtTest/Bundle/UrlBundle/Url/RequestAwareProviderTest.php
+++ tests/ZichtTest/Bundle/UrlBundle/Url/RequestAwareProviderTest.php
@@ -28,10 +28,13 @@ namespace ZichtTest\Bundle\UrlBundle\Url {
             ;
             $r->expects($this->once())->method('getBaseUrl')->will($this->returnVAlue('http://example.org/foo'));
             $p = new \Zicht\Bundle\UrlBundle\Url\RequestAwareProvider($r);
-            $p->addProvider(new Provider\MockProvider(array('a' => 'b', 'c' => '/foo/a')));
+            $p->addProvider(new Provider\MockProvider(array('a' => 'b', 'c' => '/foo/a', 'd' => 'http://example.org/foo/qux')));
             $this->assertEquals('b', $p->url('a'));
-
             $this->assertEquals('http://example.org/foo/b', $p->url('a', array('absolute' => true)));
+
+            // TODO this test case fails, but not sure if this is intended...:
+//            $this->assertEquals('http://example.org/foo/a', $p->url('c', array('absolute' => true)));
+            $this->assertEquals('http://example.org/foo/qux', $p->url('d', array('absolute' => true)));
         }
     }
 }
