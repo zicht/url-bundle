e50bc15cde7bedad9d87a88ee40a2b83bb8d6b49
Boudewijn Schoon <boudewijn@zicht.nl>
2014-11-18 15:05:02 +0000

    Using different method to retrieve url, this solves the 'recursion' problem (which was not a bug after all)
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtUrlBundle/trunk@50254 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Bundle/UrlBundle/Aliasing/Aliaser.php
+++ src/Zicht/Bundle/UrlBundle/Aliasing/Aliaser.php
@@ -48,27 +48,22 @@ class Aliaser
      */
     public function createAlias($record)
     {
-        static $recursionProtection = array();
         $ret = false;
 
         $internalUrl = $this->provider->url($record);
-        $currentAlias = $this->aliasing->hasPublicAlias($internalUrl);
         $generatedAlias = $this->aliasingStrategy->generatePublicAlias($record);
-
-        // if we 've already stored this $generatedAlias, we can safely ignore this call
-        if (isset($recursionProtection[$internalUrl]) && $recursionProtection[$internalUrl] == $generatedAlias) {
+        if ($internalUrl == $this->aliasing->hasInternalAlias($generatedAlias)) {
+            // apparently there is already a publicUrl ($generatedAlias) that is associated with $record
+            // hence, no need to create a new alias.
             return $ret;
         }
 
-        if ($generatedAlias && $currentAlias != $generatedAlias) {
-            $recursionProtection[$internalUrl] = $generatedAlias;
-            $ret = $this->aliasing->addAlias(
-                $generatedAlias,
-                $internalUrl,
-                UrlAlias::REWRITE,
-                Aliasing::STRATEGY_SUFFIX
-            );
-        }
+        $ret = $this->aliasing->addAlias(
+            $generatedAlias,
+            $internalUrl,
+            UrlAlias::REWRITE,
+            Aliasing::STRATEGY_SUFFIX
+        );
 
         return $ret;
     }
