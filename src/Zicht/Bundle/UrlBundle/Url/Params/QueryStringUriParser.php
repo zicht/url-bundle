1c1f834ae503e5c9ce73716a7ec183dbdbf973f3
Philip Bergman <philip@zicht.nl>
2016-02-16 09:24:11 +0000

    add support for query strings, no separator for paramters
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtUrlBundle/branches/1.x@78844 0f35748b-8228-47f0-bbea-889afbbd9a42

---
+++ src/Zicht/Bundle/UrlBundle/Url/Params/QueryStringUriParser.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @author    Philip Bergman <philip@zicht.nl>
+ * @copyright Zicht Online <http://www.zicht.nl>
+ */
+namespace Zicht\Bundle\UrlBundle\Url\Params;
+
+/**
+ * Parser for key/value pairs in the url.
+ */
+class QueryStringUriParser extends UriParser
+{
+    /**
+     * Constructor; overridden to disable the separator parameters
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    /**
+     * Compose an URI from the passed params with the local separators
+     *
+     * @param Params $params
+     * @return string
+     */
+    public function composeUri($params)
+    {
+        $ret   = [];
+
+        foreach ($params as $param => $values) {
+            $internal = $param;
+            if ($external = $this->translateKeyOutput($param)) {
+                $param = $external;
+            }
+            $ret[$param] = [];
+            foreach ($values as $value) {
+                if ($external = $this->translateValueOutput($internal, $value)) {
+                    $value = $external;
+                }
+                $ret[$param][]= $value;
+            }
+        }
+
+        return http_build_query($ret);
+    }
+}
