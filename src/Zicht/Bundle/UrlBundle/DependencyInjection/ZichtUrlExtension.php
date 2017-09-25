d42e897c6d69505c4e7e78b19aa74a87de20efcf
philip <philip@zicht.nl>
2017-09-25 15:28:54 +0200

    fixed public url validation for updates

--- src/Zicht/Bundle/UrlBundle/DependencyInjection/ZichtUrlExtension.php
+++ src/Zicht/Bundle/UrlBundle/DependencyInjection/ZichtUrlExtension.php
@@ -74,8 +74,8 @@ class ZichtUrlExtension extends Extension
             $container->getDefinition('zicht_url.listener.strict_public_url')->replaceArgument(0, $config['strict_public_url']);
         }
 
-        if ($container->hasDefinition('zicht_url.validator.contains_unique_public_url')) {
-            $container->getDefinition('zicht_url.validator.contains_unique_public_url')->replaceArgument(1, $config['strict_public_url']);
+        if ($container->hasDefinition('zicht_url.validator.contains_url_alias')) {
+            $container->getDefinition('zicht_url.validator.contains_url_alias')->replaceArgument(1, $config['strict_public_url']);
         }
 
         $formResources = $container->getParameter('twig.form.resources');
