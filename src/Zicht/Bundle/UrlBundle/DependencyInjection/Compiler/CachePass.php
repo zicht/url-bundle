8255dbb846f9a46f9b0cd55a11afdf4282fdd00c
Jochem Klaver <jochem@zicht.nl>
2020-04-25 13:27:11 +0200

    Add FQCN aliases for services to be able to autowire them in projects

--- src/Zicht/Bundle/UrlBundle/DependencyInjection/Compiler/CachePass.php
+++ src/Zicht/Bundle/UrlBundle/DependencyInjection/Compiler/CachePass.php
@@ -7,6 +7,7 @@ namespace Zicht\Bundle\UrlBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Zicht\Bundle\UrlBundle\Url\Provider as UrlProvider;
 
 /**
  * Replaces the url provider with a cache wrapper, if enabled.
@@ -20,10 +21,12 @@ class CachePass implements CompilerPassInterface
     {
         if ($container->hasDefinition('zicht_url.cache')) {
             $container->setAlias('zicht_url.provider', 'zicht_url.cache_wrapper')->setPublic(true);
+            $container->setAlias(UrlProvider::class, 'zicht_url.cache_wrapper')->setPublic(true);
 
             $container->getDefinition('zicht_url.cache')->addTag('zicht_cache.cache_stats');
         } else {
             $container->setAlias('zicht_url.provider', 'zicht_url.provider.delegator')->setPublic(true);
+            $container->setAlias(UrlProvider::class, 'zicht_url.provider.delegator')->setPublic(true);
         }
     }
 }
