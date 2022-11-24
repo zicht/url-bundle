0ce516ce77c45c894e3166d9dba182b20784864c
Erik Trapman <erikt@fabrique.nl>
2022-11-24 16:13:30 +0100

    Fix working UrlType with Autocomplete

--- tests/Type/UrlTypeTest.php
+++ tests/Type/UrlTypeTest.php
@@ -5,20 +5,19 @@
 
 namespace ZichtTest\Bundle\UrlBundle\Type;
 
-use PHPUnit\Framework\MockObject\Generator;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Zicht\Bundle\UrlBundle\Aliasing\Aliasing;
+use Zicht\Bundle\UrlBundle\Type\UrlType;
 
 /**
- * @property \Zicht\Bundle\UrlBundle\Type\UrlType $type
+ * @property UrlType $type
  */
 class UrlTypeTest extends TestCase
 {
     public function setUp(): void
     {
         $aliasing = $this->getMockBuilder(Aliasing::class)->disableOriginalConstructor()->getMock();
-        $this->type = new \Zicht\Bundle\UrlBundle\Type\UrlType($aliasing);
+        $this->type = new UrlType($aliasing);
     }
 
     public function testGetBlockPrefix()
@@ -28,24 +27,6 @@ class UrlTypeTest extends TestCase
 
     public function testGetParent()
     {
-        $this->assertEquals(TextType::class, $this->type->getParent());
-    }
-
-    public function testFinishView()
-    {
-        $view = (new Generator())->getMock('Symfony\Component\Form\FormView');
-        $view->vars['value'] = 'foo';
-        $this->type->finishView(
-            $view,
-            (new Generator())->getMock('Symfony\Component\Form\Form', [], [], '', false),
-            [
-                'with_edit_button' => true,
-                'url_suggest' => '/admin/url/suggest',
-            ]
-        );
-
-        $this->assertArrayHasKey('url_suggest', $view->vars);
-//        $this->assertArrayHasKey('current_url_title', $view->vars);
-        $this->assertArrayHasKey('with_edit_button', $view->vars);
+        $this->assertEquals('Zicht\Bundle\AdminBundle\Form\AutocompleteType', $this->type->getParent());
     }
 }
